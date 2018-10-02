29303a74bfd3263ec53cf18979ecdbcade990688
Brian Terry <bruxengineering@gmail.com>
2018-10-02 10:45:34 -0400

    preinstall only

--- install.sh
+++ install.sh
@@ -7,13 +7,7 @@ function prereqsInstall() {
 	(bash $parent_path/prereqs_install.sh)
 }
 
-function moogInstall() {
-	(bash $parent_path/moog_${version}_install.sh)
-}
 
-supported_versions=('6.4' '6.5')
-
-while [[ true ]]; do
 	echo "
 Starting MoogSoft Package Installer
 Currently support versions ${supported_versions[@]}
@@ -21,18 +15,6 @@ Please enter a version number to confirm installation to default single server i
 
 ${bold}***** This WILL START THE INSTALL *****${unbold}
 "
-read -p "Please enter a valid version: " -r
-echo
-		for version in "${supported_versions[@]}"
-		do 
-                	if [[ $REPLY == $version ]]; then
-				prereqsInstall
-				if [[ $1 != "PreOnly" ]]; then
-					moogInstall
-				fi
-				exit
-			fi
-		done
+prereqsInstall
 
-	echo "invalid version specified: ${REPLY} - please try again or ctrl-c"
-done
+
